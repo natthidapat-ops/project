cbdf0dd91e5f4eef351826c9472645a4c5aba15e
natthidapat-ops <natthida.pat@ku.th>
2026-03-17 22:36:25 +0700

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -7,9 +7,8 @@ const path       = require('path');
 const app  = express();
 const port = 8000;
 
-// ==========================================
 // 1. Middleware Settings
-// ==========================================
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,9 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // เปิดให้เข้าถึงรูปได้จาก http://localhost:8000/uploads/ชื่อไฟล์
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// ==========================================
 // 2. Database Connection
-// ==========================================
+
 let conn = null;
 
 const initMYSQL = async () => {
@@ -37,18 +35,14 @@ const initMYSQL = async () => {
     }
 };
 
-// ==========================================
 // 3. Routes Reference
-// ==========================================
 // ส่ง app และ function ที่คืนค่า conn ไปยังไฟล์ routes ต่างๆ
 require('./routes/users')(app,    () => conn);
 require('./routes/listings')(app, () => conn);
 require('./routes/messages')(app, () => conn);
 require('./routes/comments')(app, () => conn);
 
-// ==========================================
 // 4. Start Server
-// ==========================================
 app.listen(port, async () => {
     await initMYSQL();
     console.log(`Server is running on http://localhost:${port}`);
